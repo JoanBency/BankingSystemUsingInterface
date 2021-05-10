2f1fc758b62a532205155f8a0b9238fc894f4941
Joan Bency <joan.bency@experionglobal.local>
2021-05-10 17:56:22 +0530

    First Commit

---
+++ BankingAppUsingInterface/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankingAppUsingInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\t\tBanking System");
+            Console.WriteLine("\n1.Current Account\n2.Savings Account\n3.Recurrent Account\nChoose your account type(1-3)");
+            string choice = Console.ReadLine();
+            switch(choice)
+            {
+                case "1" : CurrentAccount CA = new CurrentAccount();
+                    CA.run();
+                    break;
+                case "2" : SavingsAccount SA = new SavingsAccount();
+                    SA.run();
+                    break;
+                case "3" : RecurrentAccount RA = new RecurrentAccount();
+                    RA.run();
+                    break;
+                default : Console.WriteLine("Wrong Choice!!!!!");
+                    break;
+            }
+        }
+    }
+}
---
+++ BankingAppUsingInterface/RecurrentAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAppUsingInterface
+{
+    public class RecurrentAccount : IAccount
+    {
+        public void deposit()
+        { }
+        public void withdraw()
+        { }
+        public void getBalance()
+        { }
+        public void run()
+        {
+            Console.WriteLine("\t\t\t\t\t\tRecurrent Account");
+            Console.WriteLine("\n1.Deposit Money\n2.Withdraw Money\n3.Get Balance\nChoose your account type(1-3)");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    deposit();
+                    break;
+                case "2":
+                    withdraw();
+                    break;
+                case "3":
+                    getBalance();
+                    break;
+                default:
+                    Console.WriteLine("Wrong Choice!!!!!");
+                    break;
+            }
+        }
+    }
+}
