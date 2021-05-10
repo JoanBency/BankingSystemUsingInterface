5a51d6e79a9b0458ec933a0ed53eb4bd90e21170
Joan Bency <joan.bency@experionglobal.local>
2021-05-10 20:23:51 +0530

    Second Commit

--- BankingAppUsingInterface/RecurrentAccount.cs
+++ BankingAppUsingInterface/RecurrentAccount.cs
@@ -8,15 +8,40 @@ namespace BankingAppUsingInterface
 {
     public class RecurrentAccount : IAccount
     {
+        double amount = 0.0;
+        double balance = 0.0;
         public void deposit()
-        { }
+        {
+            Console.WriteLine("\nEnter the amount to deposit : ");
+            amount = Convert.ToDouble(Console.ReadLine());
+            balance = balance + amount;
+            Console.WriteLine("Amount Successfully Deposited");
+            run();
+        }
         public void withdraw()
-        { }
+        {
+            if (balance != 0.0)
+            {
+                Console.WriteLine("\nEnter the amount to withdraw : ");
+                amount = Convert.ToDouble(Console.ReadLine());
+                balance = balance - amount;
+                Console.WriteLine("Amount Successfully Withdrawn");
+                run();
+            }
+            else
+            {
+                Console.WriteLine("Account is Empty");
+                run();
+            }
+        }
         public void getBalance()
-        { }
+        {
+            Console.WriteLine($"Current balance is ${balance}");
+            run();
+        }
         public void run()
         {
-            Console.WriteLine("\t\t\t\t\t\tRecurrent Account");
+            Console.WriteLine("\n\t\t\t\t\t\tRecurrent Account");
             Console.WriteLine("\n1.Deposit Money\n2.Withdraw Money\n3.Get Balance\nChoose your account type(1-3)");
             string choice = Console.ReadLine();
             switch (choice)
